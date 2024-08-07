ea8bf7c6343164c533431c92d9fceb4aae644b0e
SatyaRy <89769507+SatyaRy@users.noreply.github.com>
2024-08-07 14:51:42 +0700

    Update notification.js

--- src/notification.js
+++ src/notification.js
@@ -1,6 +1,6 @@
 function updateMe(){
     if(!("Notification" in window)){
-        console.log("the notification not support")
+        alert("the notification not support")
     }
     else if(Notification.permission ==="granted"){
         const notifyMe = new Notification("Hey")
